6dbcfa8c21a97b10dc804b920fe26da2e28080b5
Kapil <kapil@Kapil>
2020-08-12 16:11:57 +0530

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import NotesScreenComponent from './src/NotesScreenComponent';
+import firebase from 'firebase';
+import LoginScreenComponent from './src/LoginScreenComponent';
 
 export default function App() {
+  const [userLoggedIn, setUserLoggedIn] = useState(false)
+  
+  if(firebase.apps.length === 0){
+    var firebaseConfig = {
+      apiKey: "",
+      authDomain: "notes-app-3c804.firebaseapp.com",
+      databaseURL: "https://notes-app-3c804.firebaseio.com",
+      projectId: "notes-app-3c804",
+      storageBucket: "notes-app-3c804.appspot.com",
+      messagingSenderId: "1047237587814",
+      appId: "1:1047237587814:web:b0552678f70d36b9a20ea7"
+    };
+  
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
+
+  firebase.auth().onAuthStateChanged((user) => {
+    if(user === null) {
+      setUserLoggedIn(false)
+    } else {
+      setUserLoggedIn(true)
+    }
+  })
+
+  if(userLoggedIn) {
+    return (
+      <View style={styles.container}>
+        <NotesScreenComponent/>
+      </View>
+    );
+  } else {
+    return (
+      <View style={styles.container}>
+        <LoginScreenComponent/>
+      </View>
+    );
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* <NotesScreenComponent/> */}
+      <LoginScreenComponent/>
     </View>
   );
 }
